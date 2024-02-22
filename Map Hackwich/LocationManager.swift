29a7caf49a3f7a7060e159d5e8b6ae2fdb92cacb
Ethan Davis <edavis@bsd220.org>
2024-02-22 09:42:12 -0600

    added user location

---
+++ Map Hackwich/LocationManager.swift
@@ -0,0 +1,18 @@
+//
+//  LocationManager.swift
+//  Map Hackwich
+//
+//  Created by Ethan Davis on 2/22/24.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
